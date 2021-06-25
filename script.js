3779ab06b313c5e8a2b37cc1c0ce5ce34246f1c9
Rahul Ranjan Pandey <62777073+rahulranjanpandey@users.noreply.github.com>
2021-06-25 23:34:05 +0530

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+
+function printHistory(num){
+    document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
